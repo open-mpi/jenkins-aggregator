a520af28aa85eb7d894e82f14e88a192613df4eb
Natalie Chrien <nchrien@lanl.gov>
2015-04-30 10:31:52 -0600

    made a bunch of smallish changes; fleshed things out a bit

--- aggregator/views.py
+++ aggregator/views.py
@@ -6,14 +6,13 @@ from .models import Commit, ResultSet, TestResult
 class IndexView(generic.ListView):
     template_name = "aggregator/index.html"
     context_object_name = "latest_commit_list"
-
-    def get_queryset(self):
-        return Commit.objects.order_by("-triggerTime")[:5]
+    queryset = Commit.objects.order_by("-triggerTime")[:5] # TODO any reason to use get_queryset() instead?
         
 
 class CommitView(generic.DetailView):
     model = Commit
     template_name = "aggregator/commit.html"
+    
     
 class ResultSetView(generic.DetailView):
     model = ResultSet

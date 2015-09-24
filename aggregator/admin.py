f70b3045461f69610f21b167047f6d6ceb5acd58
Natalie Chrien <nchrien@lanl.gov>
2015-09-24 14:04:20 -0600

    removed old Django stuff

--- aggregator/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Commit, ResultSet
-
-admin.site.register(Commit)
-admin.site.register(ResultSet)
-
-admin.AdminSite.site_header = "Jenkins Aggregator Administration"
--- aggregator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r"^$", views.IndexView.as_view(), name="index"),
-    url(r"^commit/(?P<pk>[0-9]+)/$", views.CommitView.as_view(), name="commit"), # TODO better name in URL than "commit/"?
-    url(r"^result/(?P<pk>[0-9]+)/$", views.ResultSetView.as_view(), name="resultset"), # TODO make it include the Commit's ID/URL (e.g., commit/x/result/y/)?
-]
